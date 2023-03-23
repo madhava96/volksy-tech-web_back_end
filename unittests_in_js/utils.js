f5ad3d544756b5ddb04c787f68cb85ce729568d4
madhava96 <madhavas@brightcomgroup.com>
2023-03-23 10:35:00 +0000

    task3

--- unittests_in_js/utils.js
+++ unittests_in_js/utils.js
@@ -5,4 +5,5 @@ const Utils = {
 	if (type === 'SUBTRACT') return Math.round(a) - Math.round(b)
 	if (type === 'DIVIDE') return Math.round(b) === 0 ? 'Error' : Math.round(a) / Math.round(b);
 }
+}
 module.exports = Utils;
