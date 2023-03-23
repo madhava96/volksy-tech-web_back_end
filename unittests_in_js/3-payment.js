45c2ea1c9a7885b82044e2ce5d5b3335a6968323
madhava96 <madhavas@brightcomgroup.com>
2023-03-23 10:27:32 +0000

    task3

--- unittests_in_js/3-payment.js
+++ unittests_in_js/3-payment.js
@@ -2,7 +2,7 @@
 const Utils = require('./utils.js');
 
 function sendPaymentRequestToApi(totalAmount, totalShipping) {
-const res = Utils.calculateNumber('SUM', totalAmount, totalShipping);
+	const res = Utils.calculateNumber('SUM', totalAmount, totalShipping);
 	console.log('The total is:${res}');
 };
-module.exports = sendPaymentRequestToApi; 
+module.exports = sendPaymentRequestToApi;
