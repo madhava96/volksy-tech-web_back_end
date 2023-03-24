8663494161aa3f1b43faab6362b511368cd6a7a1
madhava96 <madhavas@brightcomgroup.com>
2023-03-24 05:44:29 +0000

    task 6

--- unittests_in_js/6-payment_token.js
+++ unittests_in_js/6-payment_token.js
@@ -1,5 +1,5 @@
 'use strict';
-function getPaymentTokenFromAPI(sucess) {
-	if (sucess) return promise.resolve({ data: 'sucessful response from the API'});
+function getPaymentTokenFromAPI(success) {
+	if (success) return Promise.resolve({ data: 'Successful response from the API'});
 }
 module.exports = getPaymentTokenFromAPI;
